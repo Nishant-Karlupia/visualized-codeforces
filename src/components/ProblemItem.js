874285b50e7286608ea845815ab5a3ac85b1c1e9
Nishant Karlupia <nishantkarlupia149@gmail.com>
2023-08-07 18:30:03 +0530

    added solution links

--- src/components/ProblemItem.js
+++ src/components/ProblemItem.js
@@ -1,18 +1,47 @@
-import React from 'react'
+import React from "react";
 
 const ProblemItem = (props) => {
   return (
-    <div className='container' style={{"maxWidth":"800px"}}>
-        <div className=' stats-solved-container'>
-          <div >
-            {props.rating} | {props.pindex} . {props.pname}
-          </div>
-          <div style={{"border":"0px solid green"}}>
-            <a href={`https://codeforces.com/contest/${props.contest}/submission/${props.subId}`} style={{ "textDecoration": "none" }} target="_blank" rel="noreferrer">{props.subId}</a>
-          </div>
+    <div
+      className="container"
+      style={{ maxWidth: "800px", border: "0px solid red" }}
+    >
+      <div className=" stats-solved-container">
+        <div>
+          <span style={{ border: "0px solid green" }}>
+            <a
+              href={`https://codeforces.com/problemset/status/${props.contest}/problem/${props.pindex}`}
+              style={{ textDecoration: "none" }}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {props.rating}
+            </a>
+          </span>{" "}
+          | {props.pindex} . <span style={{ border: "0px solid green" }}>
+            <a
+              href={`https://codeforces.com/problemset/problem/${props.contest}/${props.pindex}`}
+              style={{ textDecoration: "none" }}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {props.pname}
+            </a>
+          </span>
         </div>
+        <div style={{ border: "0px solid green" }}>
+          <a
+            href={`https://codeforces.com/contest/${props.contest}/submission/${props.subId}`}
+            style={{ textDecoration: "none" }}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {props.subId}
+          </a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProblemItem
+export default ProblemItem;
